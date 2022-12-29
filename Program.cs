4de290279ebc6392dd943cef7d95578b3fff0856
Mohamedshaaban12 <121443638+Mohamedshaaban12@users.noreply.github.com>
2022-12-29 16:42:12 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,55 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
 
-namespace project_1
+namespace project2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            int n1, n2, i, flag, temp;
-            // take an int as an input from user to start counting from
-            Console.WriteLine("Enter The Number To Start Counting From");
-            n1 = int.Parse(Console.ReadLine());
+            // take as int as an from to start counting from
+            Console.WriteLine("Enter The Number To Start Counting from:");
+            int Number1 = int.Parse(Console.ReadLine());
 
-            // take an int as an input from user to stop counting from
-            Console.WriteLine("Enter The Number To Stop Counting From");
-            n2 = int.Parse(Console.ReadLine());
+            // take as int as an from to stop counting from
+            Console.WriteLine("Enter The Number To Stop Counting from:");
+            int Number2 = int.Parse(Console.ReadLine());
 
-            if (n1 > n2)
+            //an outer loop that start from number1 to number2
+            for(int c = Number1; c <= Number2; c++)
             {
-                temp = n1;
-                n1 = n2;
-                n2 = temp;
-            }
-            // print statemet(prime number betweeen n1 and n2)
-            Console.WriteLine("prime Number Between"+n1+" and"+n2);
-            while (n1 < n2)
-            {
-
-
-                flag = 0;
-                for (i = 2; i <= n1 / 2; i++)
+                //anew variable that sum up the factors of the number(which is c) that the outer loop stoped at (the number itself wont be added up)
+                int sum = 0;
+                /* an inner for loop that start counting from 1 to bellow the number that c stopped at by 1
+                 so each time c increases the inner loop start all over again*/
+                for(int d = 1; d < c; d++)
                 {
-                    if (n1 % i == 0)
+                    //if the number is disisible by another number, add that number to sum
+                    if (c % d == 0)
                     {
-                        flag = 1;
-                        break;
+                        sum += d;
                     }
-                }
-                if(flag == 0)
-                
-                    Console.WriteLine("\t"+n1);
-                    n1++;
+                    else
+                    {
+                        continue;
+                }   }
+                    //if sum equal to c, then the number is perfect
+                    if (sum == c)
+                    {
+                        Console.WriteLine(c);
+                    }
+                        
                 
             }
-            
+
 
         }
     }
